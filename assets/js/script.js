962e5ccd2dcc99c11ef086dc0ff36f5b8bb390e8
Kate Llewellyn <llewellyn.ksj@gmail.com>
2022-12-30 11:12:54 +0000

    Add getWord function to pull a random word and passing it a parameter so that different arrays can be used in the checkLevel function. Also added core variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
+// variables
+const theWord = document.getElementById("the-word");
+const userAnswer = document.getElementById("user-answer");
+let randomWord;
+
 function startGame() {
 
 }
 
-function getWord() {
-
+// get a random word from the array. Passing a parameter so that different arrays can be used dependent on level
+function getWord(array) {
+    randomWord = array[Math.floor(Math.random() * array.length)];
+    return randomWord;
 }
 
 function jumbleLetters() {
