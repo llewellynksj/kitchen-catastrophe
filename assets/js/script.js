10ad2cd64ebbf0c410309c1c2c3eec87e75aa957
Kate Llewellyn <llewellyn.ksj@gmail.com>
2022-12-30 11:03:01 +0000

    Add core function names to script

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function startGame() {
+
+}
+
+function getWord() {
+
+}
+
+function jumbleLetters() {
+
+}
+
+function checkLevel() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function displayStats() {
+
+}
+
+function resetAnswerBox() {
+
+}
