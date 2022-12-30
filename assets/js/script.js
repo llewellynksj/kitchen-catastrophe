975cce1cd6f8da7f9fa63fec801441fb59602a18
Kate Llewellyn <llewellyn.ksj@gmail.com>
2022-12-30 11:20:22 +0000

    Add jumbleLetters function to scramble the letters of the random word taken from the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,27 @@ function startGame() {
 
 }
 
-// get a random word from the array. Passing a parameter so that different arrays can be used dependent on level
+/** Get a random word from the array. Passing a parameter so that different arrays can be used dependent on level */
 function getWord(array) {
     randomWord = array[Math.floor(Math.random() * array.length)];
     return randomWord;
 }
 
-function jumbleLetters() {
+/** Scramble the letters of the word that has been taken at random from the array  */
+function jumbleLetters(randomWord) {
+
+    // splitting the letters of the random word
+    let letterMix = randomWord.split("");
+
+    // shuffling the letters using Fisher-Yates algorithm (code taken from James Bubb: https://bit.ly/3XZ3S6y)
+    for (let i = letterMix.length - 1; i > 0; i--) {
+       let j = Math.floor(Math.random() * (i + 1));
+       const temp = letterMix[i];
+       letterMix[i] = letterMix[j];
+       letterMix[j] = temp;
+    }
+    
+    return letterMix.join("");
 
 }
 
